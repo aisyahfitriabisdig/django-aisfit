af1ec432eda6027fbe401231e4827f32830988f5
aisyahfitriabisdig <148046525+aisyahfitriabisdig@users.noreply.github.com>
2024-05-03 01:03:18 +0800

    Initial commite

--- aisfitsite/aisfitsite/urls.py
+++ aisfitsite/aisfitsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ais_site project.
+URL configuration for aisfitsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
